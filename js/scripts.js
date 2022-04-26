8a3cf3cf169a3e4e01f96c28e2bf7ca176c6113b
Eli <elibrumley@gmail.com>
2022-04-25 23:42:14 -0500

    case studies + about pages : build out

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Scroll add header class
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
 
@@ -8,3 +9,19 @@ $(window).scroll(function() {
     }
 });
 
+// Isitope Init script
+ 
+$('.grid').isotope({
+    itemSelector: '.grid-item',
+    percentPosition: true,
+    masonry: {
+      columnWidth: '.grid-sizer'
+    }
+});
+// filter items on button click
+$('.filters').on( 'click', 'li', function() {
+    var filterValue = $(this).attr('data-filter');
+    $('.grid').isotope({ filter: filterValue });
+    $('.filters li').removeClass('active');
+    $(this).addClass('active');
+});
